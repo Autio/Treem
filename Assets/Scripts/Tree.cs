afe5301da8cb6ca564971d4991661e051ed884c0
Petri Autio <petri.mikael.autio@gmail.com>
2017-10-21 20:01:08 +0100

    Gamedev notes

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -1,51 +1,79 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Tree : MonoBehaviour {
 
-    /*
-     * A tree clicker game
-     * 
-     * Two levels of play: Individual tree level and the forest level
-     * Players can control three parts of the tree in two dimensions each
-     * Roots can be either extended or convoluted
-     * The branches can be extended or convoluted
-     * The trunk can be long and thin or shorter and thicker
-     * 
-     * The growing environment impacts which kind of trees thrive best
-     * 
-     * The pace of the game is casual and gentle
-     * 
-     * Visually there should be buttons on the sides of the mobile screen 
-     * A box of key stats on the top
-     * 
-     * It would be great if the tree itself was a voxel one, procedurally generated based on the choices 
-     * 
-     * 
-     * At the higher forest level there should be delineated circle domains each with their own characteristics
-     * which are combination of three factors (?) perhaps wetness, sunshine and soil quality
-     * 
-     * /
+     //* A tree clicker game
+     //* 
+     //* Two levels of play: Individual tree level and the forest level
+     //* Players can control three parts of the tree in two dimensions each
+     //* Roots can be either extended or convoluted
+     //* The branches can be extended or convoluted
+     //* The trunk can be long and thin or shorter and thicker
+     //* 
+     //* The growing environment impacts which kind of trees thrive best
+     //* 
+     //* The pace of the game is casual and gentle
+     //* 
+     //* Visually there should be buttons on the sides of the mobile screen 
+     //* A box of key stats on the top
+     //* 
+     //* It would be great if the tree itself was a voxel one, procedurally generated based on the choices 
+     //* 
+     //* 
+     //* At the higher forest level there should be delineated circle domains each with their own characteristics
+     //* which are combination of three factors (?) perhaps wetness, sunshine and soil quality
+     //* 
+  
+    int roots = 0;
+    int rootLength = 0;
+    int rootConvolution = 0;
+    int trunk = 0;
+    int branches = 0;
 
-    class tree ()
+    public int Branches
     {
-        int roots = 0;
-        int trunk = 0;
-        int branches = 0;
+        get
+        {
+            return branches;
+        }
 
-        
-
+        set
+        {
+            branches = value;
+        }
     }
 
+    public int Trunk
+    {
+        get
+        {
+            return trunk;
+        }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        set
+        {
+            trunk = value;
+        }
+    }
+
+    public int RootLength
+    {
+        get
+        {
+            return rootLength;
+        }
+
+        set
+        {
+            rootLength = value;
+        }
+    }
+        
+    void AddToRoot()
+    {
+        roots += 1;
+    }
+        
+    
+    
 }
